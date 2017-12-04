e4912e7f6ef96b65fdd5840176c1a277080bf18a
nehemiahnewell <nehemiah.newell@gmail.com>
2017-12-04 16:37:44 +0000

    working

--- sep-assignments-master/01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ sep-assignments-master/01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,39 @@ class Line
   end
 
   def join(person)
+    self.members.push(person)
   end
 
   def leave(person)
+    self.members.delete_if { |a| a == person }
   end
 
   def front
+    self.members.first
   end
 
   def middle
+    length = self.members.length
+    self.members[((length/2).ceil)]
   end
 
   def back
+    self.members.last
   end
 
   def search(person)
+    self.members.each do |name|
+      if name == person
+        return name
+      end
+    end
+    nil
   end
 
   private
 
   def index(person)
+    self.members.index(person)
   end
 
 end
