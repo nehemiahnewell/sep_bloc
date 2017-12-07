9819eada5f3ce8013a659e9a7b5bbd937e679e08
nehemiahnewell <nehemiah.newell@gmail.com>
2017-12-07 06:48:18 +0000

    working, but I'm not sure the logic is valid

--- sep-assignments-master/01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ sep-assignments-master/01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,26 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue.insert(@queue.size, element)
+    @tail = @queue[@queue.size - 1]
+    if(@head == nil)
+      @head = @tail
+    end
   end
 
   def dequeue
+    returnvalue = @head
+    @head = @queue.fetch(1, nil)
+    @queue.delete_at(0)
+    @queue.compact!
+    return returnvalue
   end
 
   def empty?
+    if(@queue.size > 0)
+      return false
+    else
+      return true
+    end
   end
 end
