185b2c3e4a4974893cb9e5ec56c16607f2fea7d7
nehemiahnewell <nehemiah.newell@gmail.com>
2017-12-11 07:43:48 +0000

    missed

--- sep-assignments-master/01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ sep-assignments-master/01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,22 @@ class MyStack
   end
 
   def push(item)
+    @stack.insert(@stack.size, item)
+    self.top = @stack[@stack.size - 1]
   end
 
   def pop
+    @stack.delete_at(@stack.size - 1)
+    returnvalue = self.top
+    self.top = @stack[@stack.size - 1]
+    return returnvalue
   end
 
   def empty?
+    if(@stack.size > 0)
+      return false
+    else
+      return true
+    end
   end
 end
